1438c3a0a8e6bcb533272400b2e6155209b05ffe
IkarusVI <tomasdimartinobuduan@gmail.com>
2024-02-20 15:02:34 +0100

    Dark Mode added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  darkMode: boolean = false;
+  imgMode: string = '../assets/moon.svg';
+
+  changeDarkMode = () => {
+    const mode=document.documentElement.classList.toggle("dark");
+    if (mode) {
+      this.imgMode = '../assets/sun.svg'; // Dark mode is enabled
+    } else {
+      this.imgMode = '../assets/moon.svg'; // Dark mode is disabled
+    }
+  }
 }
